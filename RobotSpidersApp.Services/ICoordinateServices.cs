904e97bfd06774f0edaad934d85a52285ad9c235
santanu82 <santanu1982@ymail.com>
2020-02-19 11:58:30 +0000

    Add files via upload

---
+++ RobotSpidersApp.Services/ICoordinateServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSpidersApp.Services
+{
+    public interface ICoordinateServices
+    {
+        int RobotFinalCoordinatesWhenInstructionIsM(char robotInitialOrientation, int robotInitialXCoordinate,
+            ref int robotInitialYCoordinate);
+    }
+}
---
+++ RobotSpidersApp.Services/IRobotServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSpidersApp.Services
+{
+    public interface IRobotServices 
+    {
+        Tuple<int, int, char> GetRobotFinalCoordinateAndOrientation(Tuple<int, int> robotMaxCoordinates,
+            Tuple<int, int, char> RobotInitialCoordinatesAndOrientation, string robotInstruction);
+    }
+}
